0605fa208a93945d2c64c18e0d4e6fadcfe4b805
novitageraldine <novitageraldine@users.noreply.github.com>
2015-09-13 10:09:37 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+# 18213042-18213043
+Tugas Kelompok Pemrograman Integratif
+
+import SocketServer
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 8088
+
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    server.serve_forever()
