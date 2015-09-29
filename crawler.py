a1d4175e928c0fd6d650b3325734c4f891bff446
novitageraldine <novitageraldine@users.noreply.github.com>
2015-09-29 16:02:44 +0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,12 +1,63 @@
-import re, urllib
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-textfile = file('depth_1.txt','wt')
-print "Enter the URL you wish to crawl.."
-print 'Usage  - "http://phocks.org/stumble/creepy/" <-- With the double quotes'
-myurl = input("@> ")
-for i in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(myurl).read(), re.I):
-        print i  
-#       for ee in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(i).read(), re.I):
-#       	print ee
-        textfile.write(i+'\n')
-textfile.close()
+public class WebpagesDownloader {
+   public static List<String> getHyperlinks(String url) {
+      List<String> urls = new ArrayList<String>();
+      try {
+         Document doc = Jsoup.connect(url).get();
+         Elements links = doc.select("a[href]");
+         for(Element l: links){
+              urls.add(l.attr("abs:href"));
+         }              
+      } catch(IOException e) {
+         e.printStackTrace();
+      }
+      return urls;
+   }
+
+   public static void printHyperlinks(List<String> links) {
+      for(String link : links) {
+         System.out.println("link: " + link);
+      }
+   }
+
+   public static void downloadWebpage(String link, String data) throws Exception {
+      URL url = new URL(link);
+      BufferedReader reader = new BufferedReader
+      (new InputStreamReader(url.openStream()));
+      BufferedWriter writer = new BufferedWriter
+      (new FileWriter(data));
+      String line;
+      while ((line = reader.readLine()) != null) {
+         writer.write(line);
+         writer.newLine();
+      }
+      reader.close();
+      writer.close();
+   }
+
+   public static void main (String[] args) {
+      int counter = 0;
+      try {
+         downloadWebpage("http://www.sp.itb.ac.id/", "Data.html");      
+      } catch(Exception e) {
+         e.printStackTrace();
+      }
+      List<String> urls = getHyperlinks("http://www.sp.itb.ac.id/");
+      printHyperlinks(urls);
+   
+     for(String url : urls) {
+         try {
+            downloadWebpage(url, "SubData" + counter + ".html");      
+         } catch(Exception e) {
+            e.printStackTrace();
+         }      
+      }
+   }
+}
